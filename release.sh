640c835e45d5d615e58a9485975c29533af01ba1
kmakris23 <kostasmakris23@gmail.com>
2024-09-06 16:37:23 +0300

    fix(workflow): update release workflow

--- release.sh
+++ release.sh
@@ -3,9 +3,9 @@
 echo "::set-output name=new_release_version::$1"
 
 # Querier
-sed -i "s#<PackageVersion>.*#<PackageVersion>$1</PackageVersion>#" Querier/Querier.csproj
-sed -i "s#<Version>.*#<Version>$1</Version>#" Querier/Querier.csproj
+sed -i "s#<PackageVersion>.*#<PackageVersion>$1</PackageVersion>#" src/Querier/Querier.csproj
+sed -i "s#<Version>.*#<Version>$1</Version>#" src/Querier/Querier.csproj
 
 # Querier.SqlQuery
-sed -i "s#<PackageVersion>.*#<PackageVersion>$1</PackageVersion>#" Querier.SqlQuery/Querier.SqlQuery.csproj
-sed -i "s#<Version>.*#<Version>$1</Version>#" Querier.SqlQuery/Querier.SqlQuery.csproj
+sed -i "s#<PackageVersion>.*#<PackageVersion>$1</PackageVersion>#" src/Querier.SqlQuery/Querier.SqlQuery.csproj
+sed -i "s#<Version>.*#<Version>$1</Version>#" src/Querier.SqlQuery/Querier.SqlQuery.csproj
