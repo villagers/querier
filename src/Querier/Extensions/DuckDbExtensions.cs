2a6d83241915e80734055c6a5ac1c9f49342da3b
kmakris23 <kostasmakris23@gmail.com>
2024-09-28 20:41:28 +0300

    fix: remove extra parameters

--- src/Querier/Extensions/DuckDbExtensions.cs
+++ src/Querier/Extensions/DuckDbExtensions.cs
@@ -11,7 +11,7 @@ namespace Querier.Extensions
 {
     public static class DuckDbExtensions
     {
-        public static DuckDBAppenderRow AppendValue(this DuckDBAppenderRow row, object value, int fieldIndex, string fieldName)
+        public static DuckDBAppenderRow AppendValue(this DuckDBAppenderRow row, object value)
         {
             var type = value.GetType();
             if (value == null) return row.AppendNullValue();
